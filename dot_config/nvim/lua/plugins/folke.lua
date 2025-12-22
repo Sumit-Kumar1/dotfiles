41d200900a44b36fc9dd6ba612f3e8cfd3b3e50b
Sumit Kumar <kumarsumitjat298@gmail.com>
2025-12-22 18:29:46 +0530

    updated nvim template with folke lazyvim

---
+++ dot_config/nvim/lua/plugins/folke.lua
@@ -0,0 +1,16 @@
+return {
+  { "folke/neodev.nvim" },
+  { "folke/which-key.nvim",     lazy = true },
+  { "folke/neoconf.nvim",       cmd = "Neoconf" },
+  { "folke/todo-comments.nvim", opts = {} },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  { "folke/noice.nvim", opts = {} },
+}
---
+++ dot_config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        keys = {
+            { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+        },
+        opts = {},
+    }
+}
