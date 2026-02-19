7fbd67a7a7eecc50ba52798e6726867b1b6436b0
Sumit Kumar <kumarsumitjat298@gmail.com>
2026-02-19 19:12:59 +0530

    updated nvim and kitty configs

--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -1,76 +1,42 @@
 return {
   -- add gruvbox
-  -- { "ellisonleao/gruvbox.nvim" },
-  -- Configure LazyVim to load gruvbox
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = true,
-  --   opts = { style = "moon" },
-  -- },
-  -- {
-  --   "ray-x/starry.nvim",
-  --   opts = {
-  --     italics = {
-  --       comments = true,
-  --       strings = false,
-  --       keywords = false,
-  --       functions = false,
-  --       variables = false,
-  --     },
-  --     contrast = {
-  --       enable = true,
-  --       terminal = true,
-  --     },
-  --     style = {
-  --       name = "moonlight", -- other themes: dracula, oceanic, dracula_blood, 'deep ocean', darker, palenight, monokai, mariana, emerald, middlenight_blue
-  --       disable = {}, -- a list of styles to disable
-  --       fix = true,
-  --       darker_contrast = false,
-  --       daylight_switch = false,
-  --       deep_black = false,
-  --     },
-  --   },
-  -- },
   {
-    "EdenEast/nightfox.nvim",
+    "ellisonleao/gruvbox.nvim",
     opts = {
-      transparent = true,
       terminal_colors = true,
-      dim_inactive = true,
-      styles = { -- Style to be applied to different syntax groups
-        comments = "italics", -- Value is any valid attr-list value `:help attr-list`
-        conditionals = "NONE",
-        constants = "NONE",
-        functions = "NONE",
-        keywords = "NONE",
-        numbers = "NONE",
-        operators = "NONE",
-        strings = "italics",
-        types = "NONE",
-        variables = "NONE",
+      underline = true,
+      undercurl = true,
+      bold = true,
+      italic = {
+        string = true,
+        emphasis = true,
+        comments = true,
+        operators = true,
+        folds = true,
       },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      inverse = false, -- invert background for search, diffs, statuslines and errors
+      contrast = "soft", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,
     },
   },
-  -- {
-  --   "kuri-sun/yoda.nvim",
-  --   opts = {
-  --     neo_tree = true,
-  --     telescope = false,
-  --     gitsigns = true,
-  --     snacks = true,
-  --     indent_blankline = true,
-  --     dashboard = true,
-  --     which_key = true,
-  --     misc = true,
-  --     mini = true,
-  --     noice = true,
-  --     trouble = true,
-  --   },
-  -- },
+  -- Configure LazyVim to load gruvbox
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+  -- set theme globally
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "carbonfox",
+      colorscheme = "gruvbox",
     },
   },
 }
